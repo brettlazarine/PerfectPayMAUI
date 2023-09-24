5bbe5538e9fb785a80082c55644525cd43bd0ff0
Brett Lazarine <jblazarine@gmail.com>
2023-09-24 14:17:25 -0500

    complete project

--- PerfectPay/PerfectPay/MainPage.xaml.cs
+++ PerfectPay/PerfectPay/MainPage.xaml.cs
@@ -2,23 +2,69 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
+	double bill;
+	int tip;
+	int noPersons = 1;
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+    private void txtBill_Completed(object sender, EventArgs e)
+    {
+		bill = double.Parse(txtBill.Text);
+
+		CalculateTotal();
+    }
+
+	private void CalculateTotal()
 	{
-		count++;
+		var totalTip = (bill * tip) / 100;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		var tipByPerson = totalTip / noPersons;
+		lblTipByPerson.Text = $"{tipByPerson:C}";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		var subtotal = bill / noPersons;
+		lblSubtotal.Text = $"{subtotal:C}";
+
+		var totalByPerson = (bill + totalTip) / noPersons;
+		lblTotal.Text = $"{totalByPerson:C}";
 	}
+
+    private void sldTip_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+		tip = (int)sldTip.Value;
+
+		lblTip.Text = $"Tip: {tip}%";
+
+		CalculateTotal();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		if (sender is Button)
+		{
+			Button btn = (Button)sender;
+			var percentage = int.Parse(btn.Text.Replace("%", ""));
+			sldTip.Value = percentage;
+		}
+    }
+
+    private void btnMinus_Clicked(object sender, EventArgs e)
+    {
+		if (noPersons > 1)
+		{
+			noPersons--;
+		}
+        lblNoPersons.Text = noPersons.ToString();
+        CalculateTotal();
+    }
+
+    private void btnPlus_Clicked(object sender, EventArgs e)
+    {
+		noPersons++;
+		lblNoPersons.Text = noPersons.ToString();
+		CalculateTotal();
+    }
 }
 
